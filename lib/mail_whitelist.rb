1217a284789823f2df512bdf5042d96c7833dcc2
Tom Kruijsen <tom@tomkruijsen.nl>
2017-01-05 16:49:56 +0100

    Add initial functionality
    This has been extracted from a combination of Brightin projects.

--- lib/mail_whitelist.rb
+++ lib/mail_whitelist.rb
@@ -3,4 +3,19 @@ require 'mail_whitelist/version'
 # Filter mails with a specific whitelist of e-mailaddresses and only leaves
 # those in the 'to'.
 class MailWhitelist
+  attr_reader :whitelist, :fallback
+
+  # @param [Array<String>, #include?] whitelist
+  # @param [String] fallback
+  def initialize(whitelist, fallback = nil)
+    @whitelist = whitelist
+    @fallback = fallback
+  end
+
+  def delivering_email(mail)
+    mail.to = mail.to.select do |recipient|
+      @whitelist.include?(recipient)
+    end
+    mail.to = [fallback] unless mail.to.any?
+  end
 end
--- spec/mail_whitelist_spec.rb
+++ spec/mail_whitelist_spec.rb
@@ -1,7 +1,25 @@
 require 'spec_helper'
+require 'ostruct'
 
-describe MailWhitelist do
-  it 'has a version number' do
-    expect(MailWhitelist::VERSION).not_to be nil
+RSpec.describe MailWhitelist do
+  subject(:mail_whitelist) do
+    described_class.new(['john@example.com', 'eric@example.com'],
+                        'terry@example.com')
+  end
+
+  it 'is compatible with Mail interceptors' do
+    expect(mail_whitelist).to respond_to(:delivering_email)
+  end
+
+  it 'removes recipients from an email using a whitelist' do
+    email = OpenStruct.new(to: ['john@example.com', 'graham@example.com'])
+    expect { mail_whitelist.delivering_email(email) }
+      .to change { email.to }.to(['john@example.com'])
+  end
+
+  it 'will send the email to the fallback when no recipients remain' do
+    email = OpenStruct.new(to: ['graham@example.com'])
+    expect { mail_whitelist.delivering_email(email) }
+      .to change { email.to }.to(['terry@example.com'])
   end
 end
